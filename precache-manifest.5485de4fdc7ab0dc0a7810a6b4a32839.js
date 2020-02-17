828a0da7191b380c1515e9eb30e5d2b880adaa47
Jan <kamilj_95@wp.pl>
2020-02-17 20:47:23 +0100

    Updates

---
+++ precache-manifest.5485de4fdc7ab0dc0a7810a6b4a32839.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a194ba293e611fb41628e555b46306f9",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "e9ae86f27c2cbdff147f",
+    "url": "/WeatherApp/static/css/main.7f26b1c7.chunk.css"
+  },
+  {
+    "revision": "d71975cfaf6006d55c8b",
+    "url": "/WeatherApp/static/js/2.cca7dc3e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/WeatherApp/static/js/2.cca7dc3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9ae86f27c2cbdff147f",
+    "url": "/WeatherApp/static/js/main.552895ac.chunk.js"
+  },
+  {
+    "revision": "8bbb3bf8e4a22c0a13c4",
+    "url": "/WeatherApp/static/js/runtime-main.f5a434f0.js"
+  }
+]);
